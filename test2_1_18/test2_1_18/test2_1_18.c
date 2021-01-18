aa89a9157d40162db3225d8a22bf68c06c678381
hangerQin <qdyqnn303@163.com>
2021-01-18 23:58:57 +0800

    1_18

---
+++ test2_1_18/test2_1_18/test2_1_18.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<math.h>
+
+//输出100到200的素数及其个数
+
+int main()
+{
+	int n = 0;
+	int i = 0;
+	int count = 0;
+	
+	for (i = 101; i <=200; i+=2)
+	{
+		int j = 0;
+		for (j = 2; j <=sqrt(i); j++)
+		{
+			if (i%j == 0)
+			{
+				break;
+			}		
+		}
+		if (j>sqrt(i))
+		{
+			printf(" %d ",i);
+			count++;
+		}
+	}
+	printf("count=%d", count);
+
+
+	return 0;
+}
