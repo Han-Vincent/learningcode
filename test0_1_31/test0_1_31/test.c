bdd1e5119ad7d2c3a0fe2c160406a31c87e21928
hangerQin <qdyqnn303@163.com>
2021-01-31 23:52:04 +0800

    NULL

---
+++ test0_1_31/test0_1_31/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int arr[10] = { 0 };
+	char* p = arr;
+	int i = 0;
+	for (i - 0; i < 10; i++)
+	{
+		*(p + 1) = 1;
+	}
+
+
+	return 0;
+}
